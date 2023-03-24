f79f64fd4091692dd875abcabf78aa459fc68a11
nahiataberna <taberna.127187@e.unavarra.es>
2023-03-24 16:52:04 +0100

    Añadir diferentes estilos a la app

--- App.js
+++ App.js
@@ -2,9 +2,24 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'yellow',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    text: {
+      color: 'blue',
+      textAlign: 'center',
+      fontSize: 40
+    },
+  });
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>APP NAHIA</Text>
       <StatusBar style="auto" />
     </View>
   );
